767d5532aed34b33cc93d6a7da8aad6bb10a0fb3
Victor Klochikhin <vintlucky777@gmail.com>
2016-07-06 03:06:24 +0300

    added babel compile

---
+++ app.js
@@ -0,0 +1,48 @@
+'use strict';
+
+require('babel-polyfill');
+
+var _express = require('express');
+
+var _express2 = _interopRequireDefault(_express);
+
+var _coolAsciiFaces = require('cool-ascii-faces');
+
+var _coolAsciiFaces2 = _interopRequireDefault(_coolAsciiFaces);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var app = (0, _express2.default)();
+
+app.set('port', process.env.PORT || 5000);
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.get('/cool', function (req, res) {
+  res.send((0, _coolAsciiFaces2.default)() + ' Hey man...');
+});
+
+app.get('/404', function (req, res) {
+  res.status(404);
+});
+
+app.get('/403', function (req, res) {
+  res.status(403).send({ api: 'unauthorized' });
+});
+
+app.all('/secret', function (req, res, next) {
+  res.send('Accessing the secret section ...');
+  console.log('Accessing the secret section ...');
+  next(); // pass control to the next handler
+});
+
+app.all('/*', function (req, res, next) {
+  res.send('going deeper...');
+});
+
+app.listen(app.get('port'), function () {
+  console.log('Example app listening on port ' + app.get('port') + '!');
+});
+
--- index.js
+++ index.js
@@ -1,24 +1,38 @@
+import 'babel-polyfill';
+
 import express from 'express';
+import cool from 'cool-ascii-faces';
+
 const app = express();
 
-app.get('/', function (req, res) {
-  res.status(200).send('Hello World!');
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
-app.get('/404', function (req, res) {
+app.get('/cool', (req, res) => {
+  res.send(`${cool()} Hey man...`);
+});
+
+app.get('/404', (req, res) => {
   res.status(404);
 });
 
-app.get('/403', function (req, res) {
-  res.status(404).send({api: 'unauthorized'});
+app.get('/403', (req, res) => {
+  res.status(403).send({api: 'unauthorized'});
 });
 
-app.all('/secret', function (req, res, next) {
+app.all('/secret', (req, res, next) => {
   res.send('Accessing the secret section ...');
   console.log('Accessing the secret section ...');
   next(); // pass control to the next handler
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.all('/*', (req, res, next) => {
+  res.send('going deeper...');
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`Example app listening on port ${app.get('port')}!`);
 });
